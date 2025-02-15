6c69ddcd441a73bd7fde3407fa40c768e3b35e6f
chatcopilot <chris.lee.chengdu@gmail.com>
2025-02-15 12:02:32 +0800

    a new article

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.use((req, res) => {
     res.status(404).send('Not Found');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 }); 
